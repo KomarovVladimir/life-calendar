918abef7528bef9be3d7c134e8aedd5590443497
KomarovVladimir <komarov2142@gmail.com>
2022-08-31 02:53:51 +0300

    Add a template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./scss/index.scss";
+import "./templates/index.html";
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
   },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/templates/index.html",
+    }),
+  ],
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
