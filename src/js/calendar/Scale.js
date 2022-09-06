2ff992c324cb68e66e9d1d71a75acd2cf3e812ce
KomarovVladimir <komarov2142@gmail.com>
2022-09-06 03:20:48 +0300

    Draw text on scale marks

--- src/js/calendar/Scale.js
+++ src/js/calendar/Scale.js
@@ -1,3 +1,9 @@
+//TODO: Move constants
+const scaleTypes = {
+  horizontal: "HORIZONTAL",
+  vertical: "VERTICAL",
+};
+
 export class Scale {
   #ctx;
   #step;
@@ -14,36 +20,50 @@ export class Scale {
     ctx = null,
   }) {
     this.#ctx = ctx;
+    this.#ctx.font = "16px serif";
     this.#mark = mark;
     this.#step = step;
     this.#verticalMarkersNumber = verticalMarkersNumber;
     this.#horizontalMarkersNumber = horizontalMarkersNumber;
   }
 
-  #drawHorizontalLine = (offset) => {
+  #drawLine = (offset, type) => {
     this.#ctx.beginPath();
-    this.#ctx.moveTo(0, offset);
-    this.#ctx.lineTo(this.#lineLength, offset);
+
+    if (type === scaleTypes.horizontal) {
+      this.#ctx.moveTo(0, offset);
+      this.#ctx.lineTo(this.#lineLength, offset);
+    } else if (type === scaleTypes.vertical) {
+      this.#ctx.moveTo(offset, 0);
+      this.#ctx.lineTo(offset, this.#lineLength);
+    }
+
     this.#ctx.stroke();
   };
 
-  #drawVerticalLine = (offset) => {
-    this.#ctx.beginPath();
-    this.#ctx.moveTo(offset, 0);
-    this.#ctx.lineTo(offset, this.#lineLength);
-    this.#ctx.stroke();
+  #drawNumber = (number, x, y, baseline) => {
+    this.#ctx.textBaseline = baseline;
+    this.#ctx.fillText(number, x, y);
   };
 
   draw = () => {
     for (let i = 0; i < this.#verticalMarkersNumber; i += this.#step) {
-      this.#drawVerticalLine(
-        i * this.#mark.getSize() + this.#mark.getOffset() + this.#mark.getSize()
-      );
+      const offsetX =
+        i * this.#mark.getSize() +
+        this.#mark.getSize() +
+        this.#mark.getOffset();
+
+      this.#drawLine(offsetX, scaleTypes.vertical);
+      this.#drawNumber(i, offsetX, 0, "top");
     }
     for (let i = 0; i < this.#horizontalMarkersNumber; i += this.#step) {
-      this.#drawHorizontalLine(
-        i * this.#mark.getSize() + this.#mark.getOffset() + this.#mark.getSize()
-      );
+      const offsetY =
+        i * this.#mark.getSize() +
+        this.#mark.getSize() +
+        this.#mark.getOffset();
+
+      this.#drawLine(offsetY, scaleTypes.horizontal);
+      this.#drawNumber(i, 0, offsetY, "right");
     }
   };
 }
